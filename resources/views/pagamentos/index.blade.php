f4d8ba0a672a58cdbe91e05205cfe1667ad458b5
murilovantini <m.henrique.vantini18@gmail.com>
2024-09-23 13:33:21 -0300

    feat: criar index de pagamentos. #3

--- resources/views/pagamentos/index.blade.php
+++ resources/views/pagamentos/index.blade.php
@@ -1,38 +1,73 @@
-@extends('layouts.app')
+<x-app-layout>
 
-@section('content')
-    <h1>Lista de Pagamentos</h1>
-    <a href="{{ route('pagamentos.create') }}">Adicionar Pagamento</a>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Locação</th>
-                <th>Data Pagamento</th>
-                <th>Valor Pago</th>
-                <th>Método de Pagamento</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($pagamentos as $pagamento)
-                <tr>
-                    <td>{{ $pagamento->id }}</td>
-                    <td>{{ $pagamento->locacao->id }}</td>
-                    <td>{{ $pagamento->data_pagamento }}</td>
-                    <td>{{ $pagamento->valor_pago }}</td>
-                    <td>{{ $pagamento->metodo_pagamento }}</td>
-                    <td>
-                        <a href="{{ route('pagamentos.show', $pagamento->id) }}">Mostrar</a>
-                        <a href="{{ route('pagamentos.edit', $pagamento->id) }}">Editar</a>
-                        <form action="{{ route('pagamentos.destroy', $pagamento->id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Excluir</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-@endsection
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Lista de Pagamentos') }}
+        </h2>
+        <h3 class="font-semibold text-sm underline text-gray-800 dark:text-gray-400 leading-tight hover:text-white">
+            <a href="{{ route('pagamentos.create') }}">Adicionar Pagamento</a>
+        </h3>
+    </x-slot>
+    @section('content')
+        <div class="relative overflow-x-auto mx-14 py-10">
+            <table class="w-full rounded-lg  text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Locação
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Data de Pagamento
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Valor
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Método de Pagamento
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Ações
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($pagamentos as $pagamento)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $pagamento->id }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $pagamento->locacao->id }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $pagamento->data_pagamento }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $pagamento->valor_pago }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $pagamento->metodo_pagamento }}
+                            </td>
+                            <td class="px-6 py-4">
+                                <a class="hover:underline hover:text-white"
+                                    href="{{ route('pagamentos.show', $pagamento->id) }}">Visualizar</a>
+                                <a class="hover:underline hover:text-white"
+                                    href="{{ route('pagamentos.edit', $pagamento->id) }}">Editar</a>
+                                <form action="{{ route('pagamentos.destroy', $pagamento->id) }}" method="POST"
+                                    style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="hover:underline hover:text-red-600" type="submit">Excluir</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    @endsection
+</x-app-layout>

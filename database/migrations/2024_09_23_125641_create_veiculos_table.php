4d88fdbf64562f49ed88088d5c9ac4cdae07206a
murilovantini <m.henrique.vantini18@gmail.com>
2024-09-23 10:01:15 -0300

    fix: consertar migration de veiculos

--- database/migrations/2024_09_23_125641_create_veiculos_table.php
+++ database/migrations/2024_09_23_125641_create_veiculos_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('veiculos_tables');
+        Schema::dropIfExists('veiculos');
     }
 };

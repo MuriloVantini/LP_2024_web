9a5015a4090036f9d263d0a8da69874ad0b63a0d
murilovantini <m.henrique.vantini18@gmail.com>
2024-09-23 14:49:34 -0300

    feat: criar página de cadastro de veículo. #4

--- resources/views/veiculos/create.blade.php
+++ resources/views/veiculos/create.blade.php
@@ -1,18 +1,39 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Criar Veículo</h1>
-    <form action="{{ route('veiculos.store') }}" method="POST">
-        @csrf
-        <label>Modelo:</label>
-        <input type="text" name="modelo" required>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Cadastro de Veículo') }}
+        </h2>
+    </x-slot>
+    @section('content')
+        <br><br>
+        <form class="max-w-md mx-auto" action="{{ route('veiculos.store') }}" method="POST">
+            @csrf
+            <div class="grid md:grid-cols-2 md:gap-6">
+                <div class="relative z-0 w-full mb-5 group">
+                    <input type="text" name="modelo" id="floating_modelo"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " required />
+                    <label for="floating_modelo"
+                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Modelo</label>
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <input type="text" name="marca" id="floating_marca"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " required />
+                    <label for="floating_marca"
+                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Marca</label>
+                </div>
+            </div>
+            <div class="relative z-0 w-full mb-5 group">
+                <input type="number" name="preco_diaria" id="floating_preco_diaria" step="0.01"
+                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    placeholder=" " required />
+                <label for="floating_preco_diaria"
+                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Preço da diária</label>
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar</button>
+        </form>
         
-        <label>Marca:</label>
-        <input type="text" name="marca" required>
-
-        <label>Preço Diário:</label>
-        <input type="number" name="preco_diaria" step="0.01" required>
-
-        <button type="submit">Salvar</button>
-    </form>
-@endsection
+    @endsection
+</x-app-layout>

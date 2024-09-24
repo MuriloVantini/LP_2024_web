75c60b4e5be522c8f6d60f6939c13a4b2cfd3d54
murilovantini <m.henrique.vantini18@gmail.com>
2024-09-24 11:55:52 -0300

    style: consertar título da página de cadastro de pagamento

--- resources/views/pagamentos/create.blade.php
+++ resources/views/pagamentos/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Cadastro de pagamento') }}
+            {{ __('Cadastro de Pagamento') }}
         </h2>
     </x-slot>
     @section('content')

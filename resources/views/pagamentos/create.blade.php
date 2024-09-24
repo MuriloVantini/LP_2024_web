2ab6f3af9608471257f68135d1f2aa15332c9e7a
murilovantini <m.henrique.vantini18@gmail.com>
2024-09-24 11:50:20 -0300

    cleanup: remover código comentado

--- resources/views/pagamentos/create.blade.php
+++ resources/views/pagamentos/create.blade.php
@@ -55,32 +55,3 @@
         </form>
     @endsection
 </x-app-layout>
-
-{{-- //@section('content')
-    <h1>Criar Pagamento</h1>
-    <form action="{{ route('pagamentos.store') }}" method="POST">
-        //@csrf
-
-        <label>Locação:</label>
-        <select name="locacao_id" required>
-            //@foreach ($locacoes as $locacao)
-                <option value="{{ $locacao->id }}">{{ $locacao->id }}</option>
-            //@endforeach
-        </select>
-
-        <label>Data Pagamento:</label>
-        <input type="date" name="data_pagamento" required>
-
-        <label>Valor Pago:</label>
-        <input type="number" name="valor_pago" step="0.01" required>
-
-        <label>Método de Pagamento:</label>
-        <select name="metodo_pagamento" required>
-            <option value="cartao">Cartão</option>
-            <option value="dinheiro">Dinheiro</option>
-            <option value="transferencia">Transferência</option>
-        </select>
-
-        <button type="submit">Salvar</button>
-    </form>
-@endsection --}}

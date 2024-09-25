c4447734cf5185cc47c88c867b1a53be7d4679e7
murilovantini <m.henrique.vantini18@gmail.com>
2024-09-25 11:32:35 -0300

    feat: criar edição de veículo. #6

--- resources/views/veiculos/edit.blade.php
+++ resources/views/veiculos/edit.blade.php
@@ -1,20 +1,38 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Editar Veículo</h1>
-    <form action="{{ route('veiculos.update', $veiculo->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        
-        <label>Modelo:</label>
-        <input type="text" name="modelo" value="{{ $veiculo->modelo }}" required>
-        
-        <label>Marca:</label>
-        <input type="text" name="marca" value="{{ $veiculo->marca }}" required>
-
-        <label>Preço Diário:</label>
-        <input type="number" name="preco_diaria" value="{{ $veiculo->preco_diaria }}" step="0.01" required>
-
-        <button type="submit">Salvar</button>
-    </form>
-@endsection
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Atualização de Veículo') }}
+        </h2>
+    </x-slot>
+    @section('content')
+        <br><br>
+        <form class="max-w-md mx-auto" action="{{ route('veiculos.update', $veiculo->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="grid md:grid-cols-2 md:gap-6">
+                <div class="relative z-0 w-full mb-5 group">
+                    <label for="modelo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Modelo</label>
+                    <input type="text" name="modelo" id="modelo" value="{{ $veiculo->modelo }}"
+                        class="py-2.5 px-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder=" " required />
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <label for="marca" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Marca</label>
+                    <input type="text" name="marca" id="marca" value="{{ $veiculo->marca }}"
+                        class="py-2.5 px-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder=" " required />
+                </div>
+            </div>
+            <div class="relative z-0 w-full mb-5 group">
+                <label for="preco_diaria" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preço da
+                    diária</label>
+                <input type="number" name="preco_diaria" id="preco_diaria" value="{{ $veiculo->preco_diaria }}"
+                    step="0.01"
+                    class="py-2.5 px-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=" " required />
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Atualizar</button>
+        </form>
+    @endsection
+</x-app-layout>

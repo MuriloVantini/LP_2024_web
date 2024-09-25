ea1ce76cb8b4fd15c9ddd5ee8dabf5840009643d
murilovantini <m.henrique.vantini18@gmail.com>
2024-09-25 14:18:49 -0300

    feat: criar edição de locação. #6

--- resources/views/locacoes/edit.blade.php
+++ resources/views/locacoes/edit.blade.php
@@ -1,34 +1,75 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Editar Locação</h1>
-    <form action="{{ route('locacoes.update', $locacao->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-
-        <label>Usuário:</label>
-        <select name="user_id" required>
-            @foreach ($users as $user)
-                <option value="{{ $user->id }}" {{ $user->id == $locacao->user_id ? 'selected' : '' }}>{{ $user->name }}</option>
-            @endforeach
-        </select>
-
-        <label>Veículo:</label>
-        <select name="veiculo_id" required>
-            @foreach ($veiculos as $veiculo)
-                <option value="{{ $veiculo->id }}" {{ $veiculo->id == $locacao->veiculo_id ? 'selected' : '' }}>{{ $veiculo->modelo }}</option>
-            @endforeach
-        </select>
-
-        <label>Data Início:</label>
-        <input type="date" name="data_inicio" value="{{ $locacao->data_inicio }}" required>
-
-        <label>Data Fim:</label>
-        <input type="date" name="data_fim" value="{{ $locacao->data_fim }}" required>
-
-        <label>Valor Total:</label>
-        <input type="number" name="valor_total" value="{{ $locacao->valor_total }}" step="0.01" required>
-
-        <button type="submit">Salvar</button>
-    </form>
-@endsection
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Atualização de Locação') }}
+        </h2>
+    </x-slot>
+    @section('content')
+        <br><br>
+        <form class="max-w-md mx-auto" action="{{ route('locacoes.update', $locacao->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="relative z-0 w-full mb-5 group">
+                <label for="userId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cliente</label>
+                <select name="user_id" id="userId" step="0.01"
+                    class="py-2.5 px-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=" " required>
+                    @foreach ($users as $user)
+                        <option class="dark:bg-gray-900" value="{{ $user->id }}"
+                            {{ $user->id == $locacao->user_id ? 'selected' : '' }}>{{ $user->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="relative z-0 w-full mb-5 group">
+                <label for="veiculoId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Veículo</label>
+                <select name="veiculo_id" id="veiculoId" step="0.01"
+                    class="py-2.5 px-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="" required>
+                    @foreach ($veiculos as $veiculo)
+                        <option class="dark:bg-gray-900" value="{{ $veiculo->id }}"
+                            {{ $veiculo->id == $locacao->veiculo_id ? 'selected' : '' }}>
+                            {{ $veiculo->modelo }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <label for="date-range-picker" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Data</label>
+            <div id="date-range-picker" date-rangepicker class="flex items-center mb-5">
+                <div class="relative">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                        </svg>
+                    </div>
+                    <input id="data_inicio" name="data_inicio" type="text" value="{{ $locacao->data_inicio }}"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Data inicial">
+                </div>
+                <span class="mx-4 text-gray-500">até</span>
+                <div class="relative">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                        </svg>
+                    </div>
+                    <input id="data_fim" name="data_fim" type="text" value="{{ $locacao->data_fim }}"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Data final">
+                </div>
+            </div>
+            <label for="valor_total" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Valor
+                total</label>
+            <div class="relative z-0 w-full mb-5 group">
+                <input type="number" name="valor_total" id="valor_total" value="{{ $locacao->valor_total }}"
+                    step="0.01"
+                    class="py-2.5 px-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=" " required />
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Atualizar</button>
+        </form>
+    @endsection
+</x-app-layout>

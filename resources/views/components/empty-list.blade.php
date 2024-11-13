59b468aaf43b4d534bda3e100dd8b8304cbbaba0
murilovantini <m.henrique.vantini18@gmail.com>
2024-11-13 10:18:14 -0300

    feat: criar componente para empty list (nada encontrado)

---
+++ resources/views/components/empty-list.blade.php
@@ -0,0 +1,1 @@
+<span class="text-red-600 text-lg text-center"> Nenhum dado encontrado.</span>
--- resources/views/livewire/veiculos-index.blade.php
+++ resources/views/livewire/veiculos-index.blade.php
@@ -4,6 +4,7 @@
             class="py-2.5 px-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Pesquisar por modelo ou marca" />
     </div>
+    @if (count($veiculos) > 0)
     <table class="w-full rounded-lg  text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -16,26 +17,31 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($veiculos as $veiculo)
-                <tr wire:key="veiculo-{{ $veiculo->id }}"
-                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:bg-gray-50">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {{ $veiculo->id }}
-                    </th>
-                    <td class="px-6 py-4">{{ $veiculo->modelo }}</td>
-                    <td class="px-6 py-4">{{ $veiculo->marca }}</td>
-                    <td class="px-6 py-4">R$ {{ $veiculo->preco_diaria }}</td>
-                    <td class="px-6 py-4">{{ $veiculo->disponibilidade ? 'Disponível' : 'Alugado' }}</td>
-                    <td class="px-6 py-4">
-                        <a class="hover:underline hover:text-white"
-                            href="{{ route('veiculos.show', $veiculo->id) }}">Visualizar</a>
-                        <a class="hover:underline hover:text-white"
-                            href="{{ route('veiculos.edit', $veiculo->id) }}">Editar</a>
-                        <button wire:click="deleteVeiculo({{ $veiculo->id }})"
-                            class="hover:underline hover:text-red-600">Excluir</button>
-                    </td>
-                </tr>
-            @endforeach
+            
+                @foreach ($veiculos as $veiculo)
+                    <tr wire:key="veiculo-{{ $veiculo->id }}"
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 hover:bg-gray-50">
+                        <th scope="row"
+                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $veiculo->id }}
+                        </th>
+                        <td class="px-6 py-4">{{ $veiculo->modelo }}</td>
+                        <td class="px-6 py-4">{{ $veiculo->marca }}</td>
+                        <td class="px-6 py-4">R$ {{ $veiculo->preco_diaria }}</td>
+                        <td class="px-6 py-4">{{ $veiculo->disponibilidade ? 'Disponível' : 'Alugado' }}</td>
+                        <td class="px-6 py-4">
+                            <a class="hover:underline hover:text-white"
+                                href="{{ route('veiculos.show', $veiculo->id) }}">Visualizar</a>
+                            <a class="hover:underline hover:text-white"
+                                href="{{ route('veiculos.edit', $veiculo->id) }}">Editar</a>
+                            <button wire:click="deleteVeiculo({{ $veiculo->id }})"
+                                class="hover:underline hover:text-red-600">Excluir</button>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                <x-empty-list/>
+            @endif
         </tbody>
 
     </table>

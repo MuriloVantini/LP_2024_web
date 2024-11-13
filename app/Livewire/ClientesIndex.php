dc911f938a4435225b59889a95358ec0fdf9febb
murilovantini <m.henrique.vantini18@gmail.com>
2024-11-13 11:30:38 -0300

    feat: criar filtro na listagem de cliente

--- app/Livewire/ClientesIndex.php
+++ app/Livewire/ClientesIndex.php
@@ -7,15 +7,30 @@ use Livewire\Component;
 class ClientesIndex extends Component
 {
     public $clientesAgrupados;
+    public $clientesOriginais;
+    public $pesquisa = '';
 
     // Recebe os dados do controlador
     public function mount($agrupadasPorUsuario)
     {
+        $this->clientesOriginais = $agrupadasPorUsuario;
         $this->clientesAgrupados = $agrupadasPorUsuario;
+    }
+
+    // Método para filtrar os clientes pelo nome do usuário
+    public function updatedPesquisa()
+    {
+        // Restaura a lista completa dos dados originais antes de aplicar o filtro
+        $this->clientesAgrupados = collect($this->clientesOriginais)->filter(function ($grupo) {
+            return str_contains(strtolower($grupo['usuario']->name), strtolower($this->pesquisa));
+        })->toArray();
     }
 
     public function render()
     {
-        return view('livewire.clientes-index');
+        $this->updatedPesquisa();
+        return view('livewire.clientes-index', [
+            'clientes' => $this->clientesAgrupados,
+        ]);
     }
 }
